b0cec6d1efdc836c1ff6cc51f776a7e0a1cf3ab8
Dawlatzai <dawlatzai.ghousi@gmail.com>
2022-03-26 13:21:03 +0430

    Solve exersice 6 second extra cridet challenge

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,19 +5,39 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const usernameInput = React.useRef(null)
+  const [error, setError] = React.useState(null)
   const handleSubmit = event => {
     event.preventDefault()
     const username = usernameInput.current.value
     onSubmitUsername(username)
   }
 
+  const handleChange = event => {
+    event.preventDefault()
+
+    const {value} = event.target
+
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lower case')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" ref={usernameInput} />
+        <input
+          type="text"
+          id="username"
+          ref={usernameInput}
+          onChange={handleChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <div role="alert" style={{color: 'red'}}>
+        {error}
+      </div>
+      <button type="submit" disabled={Boolean(error)}>
+        Submit
+      </button>
     </form>
   )
 }
